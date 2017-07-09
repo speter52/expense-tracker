826d78e215e547ec99c6927e5c7cd96844e7bd92
Simon Peter <simon.peter@gmail.com>
2017-07-08 22:06:56 -0700

    Initial commit with authentication and expense list/form

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'dbuser',
+        'password': 'password'
+    },
+	'database': 'expensetracker2',
+    'users_table': 'users'
+};
